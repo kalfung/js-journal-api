82af708bc65ef3d8be089a8f1e6516b3b00a5d4a
kalfung <14322@coderacademy.edu.au>
2023-08-02 13:53:52 +1000

    created POST new entry and GET single entry routes

--- index.js
+++ index.js
@@ -11,20 +11,31 @@ const entries = [
 const app = express()
 const port = 4001
 
+app.use(express.json())
+
 app.get('/', (req, res) => res.send({ info: 'Journal API' }))
 
 app.get('/categories', (req, res) => res.send(categories))
 
 app.get('/entries', (req, res) => res.send(entries))
 
+app.get('/entries/:id', (req, res) => {
+  const entry = entries[req.params.id]
+  if (entry) {
+  res.send(entry)
+  } else {
+    res.status(404).send({ error: 'Entry not found' })
+  }
+})
+
 app.post('/entries', (req, res) => {
   // 1. Retrieve data from request (req)
   console.log(req.body)
-  // 2. Parse/validate it
+  // 2. TODO: Parse/validate it
   // 3. Push the new entry to the entries array
+  entries.push(req.body)
   // 4. Send the new entry with 201 status
-  res.status(201).send('POST to /entries')
-}
-)
+  res.status(201).send(req.body)
+})
 
 app.listen(port)
