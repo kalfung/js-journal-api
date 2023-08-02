c62fee9a63012deece508158442e9a31ff6d63ab
kalfung <14322@coderacademy.edu.au>
2023-08-02 12:07:49 +1000

    created entries array and GET entries route

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ import express from 'express'
 
 const categories = ['Food', 'Gaming', 'Coding', 'Other']
 
+const entries = [
+    { category: 'Food', content: 'Dumplings are yummy!'},
+    { category: 'Coding', content: 'Lalafells enjoy coding'},
+    { category: 'Gaming', content: 'Lalafells are great at gaming'}
+  ]
+
 const app = express()
 const port = 4001
 
-app.get('/', (request, response) => response.send({ info: 'Journal API' }))
+app.get('/', (req, res) => res.send({ info: 'Journal API' }))
 
-app.get('/categories', (req, res) => res.status(204).send(categories))
+app.get('/categories', (req, res) => res.send(categories))
+
+app.get('/entries', (req, res) => res.send(entries))
 
 app.listen(port)
