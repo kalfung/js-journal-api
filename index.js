5c64bfe4d128ad8b08f23f1af4cf967957146689
kalfung <14322@coderacademy.edu.au>
2023-08-03 13:44:02 +1000

    added db connection string, created schema and model for entries, redid the POST route

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const categories = ['Food', 'Gaming', 'Coding', 'Other']
 
@@ -7,6 +8,18 @@ const entries = [
     { category: 'Coding', content: 'Lalafells enjoy coding'},
     { category: 'Gaming', content: 'Lalafells are great at gaming'}
   ]
+
+mongoose.connect('mongodb+srv://14322:kalelfung0@cluster0.s9vg5pu.mongodb.net/journal?retryWrites=true&w=majority')
+  .then(m => console.log(m.connection.readyState === 1 ? ' Mongoose connected!' : 'Mongoose failed to connect'))
+  .catch(err => console.error(err))
+
+// Making new instance of schema class
+const entriesSchema = new mongoose.Schema({
+  category: { type: String, required: true },
+  content: { type: String, required: true }
+})
+
+const EntryModel = mongoose.model('Entry', entriesSchema)
 
 const app = express()
 const port = 4001
@@ -28,14 +41,15 @@ app.get('/entries/:id', (req, res) => {
   }
 })
 
-app.post('/entries', (req, res) => {
+app.post('/entries', async (req, res) => {
   // 1. Retrieve data from request (req)
   console.log(req.body)
   // 2. TODO: Parse/validate it
   // 3. Push the new entry to the entries array
-  entries.push(req.body)
+  // entries.push(req.body)
+  const insertedEntry = await EntryModel.create(req.body)
   // 4. Send the new entry with 201 status
-  res.status(201).send(req.body)
+  res.status(201).send(insertedEntry)
 })
 
 app.listen(port)
