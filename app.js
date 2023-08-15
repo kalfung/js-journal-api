256944c7c0e97f5ff61fbf4d46658d54c493a56f
kalfung <14322@coderacademy.edu.au>
2023-08-15 10:28:08 +1000

    refactored code in index to app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import { CategoryModel, EntryModel } from './db.js'
+import entryRoutes from './routes/entry_routes.js'
+import cors from 'cors'
+
+// We could use sub-documents for one-to-many or one-to-one relationships
+// CategoryModel.create({
+//   name: 'Foo',
+//   entries: [
+//     { content: 'Bar' },
+//     { content: 'Bat' }
+//   ]
+// })
+
+// async function addEntry() {
+//   const theCat = await CategoryModel.findOne({ name: 'Coding' })
+//   EntryModel.create({ content: 'Testing category ref', category: theCat._id })
+// }
+// addEntry()
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+app.get('/', (req, res) => res.send({ info: 'Journal API' }))
+
+app.get('/categories', async (req, res) => res.send(await CategoryModel.find()))
+
+// the router is middleware, so all we say is "use that router"
+app.use('/entries', entryRoutes)
+
+export default app
